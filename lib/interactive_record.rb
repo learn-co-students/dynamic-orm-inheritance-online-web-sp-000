f6fab7d0f6eadcedc51a783d046d1a886e326186
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-02-25 21:14:18 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,7 +1,10 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+require 'pry'
 class InteractiveRecord
+
+end 
 
   def self.table_name
     self.to_s.downcase.pluralize
@@ -9,31 +12,25 @@ class InteractiveRecord
 
   def self.column_names
     DB[:conn].results_as_hash = true
-
     sql = "pragma table_info('#{table_name}')"
-
     table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
-    end
+    table_info.each {|row| column_names << row["name"]}
     column_names.compact
   end
 
-  def initialize(options={})
+  def initialize(options = {})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
 
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-  end
-
   def table_name_for_insert
     self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def values_for_insert
@@ -44,13 +41,20 @@ class InteractiveRecord
     values.join(", ")
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
-  DB[:conn].execute(sql, name)
-end
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
 
-end
+  def self.find_by(student_hash)
+    result =  student_hash.values[0]
+    col_name = student_hash.keys[0]
+    sql = "SELECT * FROM #{self.table_name} WHERE #{col_name} = ?"
+    DB[:conn].execute(sql, result)
+  end
