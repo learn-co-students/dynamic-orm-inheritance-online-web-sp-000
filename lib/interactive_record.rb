9c4955549f8abdb3c6f75c750b931ce617676763
gabejoseph <gabriel.joseph4@gmail.com>
2020-05-25 18:32:42 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,8 +1,9 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
-class InteractiveRecord
+class InteractiveRecord 
 
+  
   def self.table_name
     self.to_s.downcase.pluralize
   end
