fcaefcbed958c77a18d9871a338d033ca01e64e9
bczorn <bczorn@gmail.com>
2020-09-22 21:52:26 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,5 +5,6 @@ class Song < InteractiveRecord
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
+  #The #initialize, #save and #find_by_name methods used by Song here were inherited from InteractiveRecord.
 
 end
