ee179c484cfd12add74f6a2770adbc38c6053f1c
Sumer   Gaikwad <sg.comparch@gmail.com>
2021-01-15 06:02:41 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,7 +35,7 @@ class InteractiveRecord
   def table_name_for_insert
     self.class.table_name
   end
-
+#dsf
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
